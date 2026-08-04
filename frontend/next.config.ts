c0d2313121ede41745c028aafa6b5c9475044718
Ebakocornelius <ebakocornelius@gmail.com>
2026-08-04 09:40:37 +0100

    Remove __dirname from next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,13 +7,27 @@
 // export default nextConfig;
 
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   outputFileTracingRoot: __dirname,
+//   turbopack: {
+//     root: __dirname,
+//   },
+//   images: {
+//     remotePatterns: [
+//       { protocol: "https", hostname: "res.cloudinary.com" },
+//       { protocol: "https", hostname: "*.s3.amazonaws.com" },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: __dirname,
-  turbopack: {
-    root: __dirname,
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
